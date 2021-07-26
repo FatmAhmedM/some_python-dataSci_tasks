d2f47e2866ac2fd0d890cbd3ad1b3c5a581357e0
fuu1u <fatmaahmed0104646@sci.asu.edu.eg>
2021-07-26 15:28:02 +0200

    task

---
+++ task321.py
@@ -0,0 +1,25 @@
+print (" Welcome in calcu App") 
+print ("Use the signs + - x / to perform the basic Arithmatic operations")
+x = float(input("enter your 1st number"))
+y = input ("enter your operation sign")
+z = float(input("enter your 2nd number"))
+if y == "x":
+    print(x*z)
+elif y == "+":
+    print(x+z)
+elif y == "-":
+    print(x-z)
+elif y =="/":    
+    print(x/z)
+
+ 
+#task2
+
+    #to make an ascii represntation use the **ord("character") function**
+
+
+#task 1
+
+#c = [1,1,8,1,5,7,9]
+#for c in set(c):
+ #   print(c)
