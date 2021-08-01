753fa41de1702bf108e02e26403c390b38d4540f
fuu1u <fatmaahmed0104646@sci.asu.edu.eg>
2021-08-01 20:39:40 +0200

    t

---
+++ task3.py
@@ -0,0 +1,45 @@
+"""
+task1
+to make a prinvate varible __ will be used
+"""
+ 
+ """
+ task2
+ this is my module
+ """
+
+ def search (inpuut,input2):
+    inpuut= input("enter your text")
+    cher = list(inpuut)  
+    input2= input('enter your search character')  
+    y= cher.count(input2)
+    print(y)
+
+def calltype(n):
+   
+    no = list(n)
+    for i in n:
+        if n[2] == "2":
+            print("this is an orange's no")
+        elif n[2] == "1":
+            print("this is Etsalat no") 
+        elif n[2] == "0":
+            print("this is a vodafone no")  
+        else:
+            print("not correct") 
+
+age = 22
+
+gamescore = 99
+
+y= 85
+
+yo= 6465
+
+def calc (x):
+    print("are you here")
+    if x == "y":
+        print("welcome")
+
+
+
